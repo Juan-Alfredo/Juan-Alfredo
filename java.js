d42a92491e6d9722dee95b22d2141aaa2118bb6a
juan <juanalfredo100@gmail.com>
2021-03-14 21:24:55 -0500

    subiendo tarea

---
+++ java.js
@@ -0,0 +1,14 @@
+let btnAbout = document.querySelector("#About");
+let btnProjects = document.querySelector("#Projects");
+let btnContact = document.querySelector("#Contact");
+
+btnAbout.onclick = function () {
+    window.scrollTo(0,740);
+  };
+  btnProjects.onclick = function () {
+    window.scrollTo(0,1440);
+  };  
+  btnContact.onclick = function () {
+    window.scrollTo(0,2120);
+  };  
+
